d02a5e84a244500e3c5dbf83c24857e659761912
Sakay <farbax@hotmail.fr>
2022-02-21 14:58:05 +0100

    Formulaire Monprofil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Form\ProfilType;
 use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,5 +35,21 @@ class MainController extends AbstractController
         return $this->render("profil.html.twig", [
             'participant' => $participant
         ]);
+    }
+
+    /**
+     * @Route("monProfil/{id}", name="monProfil")
+     */
+    public function monProfil ($id, ParticipantRepository $repository, Request $request, EntityManagerInterface $entityManager) {
+     $participant = new Participant();
+     $form = $this->createForm(ProfilType::class, $participant);
+     $form->handleRequest($request);
+
+     if ($form-> isValid() && $form->isSubmitted()) {
+         $entityManager->persist($participant);
+         $entityManager->flush();
+         return $this->render("profil.html.twig");
+     }
+
     }
 }
