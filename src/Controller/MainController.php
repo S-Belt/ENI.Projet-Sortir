4a249a53f9dc5f8314d38d54aee8f38d903297f7
MehdiLarek <mehdi.larek2021@campus-eni.fr>
2022-02-21 11:48:30 +0100

    update main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="main_")
+ */
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/home.html.twig');
     }
 }
