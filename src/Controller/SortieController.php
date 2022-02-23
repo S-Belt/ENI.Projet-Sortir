f51eda08c38bbae33d5759f890024dd573ebd77a
S-Belt <beltadzes@gmail.com>
2022-02-23 10:57:04 +0100

    fonction s'inscrire aux sorties

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/sortie/sinscrire/{id}", name="sortie_sinscrire")
+     */
+    public function sinscrire(int $id, SortieRepository $sortieRepository, EntityManagerInterface $entityManager): Response
+    {
+        $sortie= $sortieRepository->find($id);
+        $participant= $this->getUser();
+        $sortie->addParticipant($participant);
+
+
+
+        $entityManager->persist($sortie);
+        $entityManager->flush();
+
+
+
+        return $this->redirectToRoute('main_afficher',['id'=>$sortie->getId()]);
+    }
+
+
+
+
+}
