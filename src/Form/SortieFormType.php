0ad3e88ff1cbbdb003f0b9ffa91421b967489d11
MehdiLarek <mehdi.larek2021@campus-eni.fr>
2022-03-02 11:33:38 +0100

    commit  null qui n'a rien changé

--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -7,18 +7,23 @@ use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Entity\Ville;
+use App\Repository\VilleRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieFormType extends AbstractType
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -42,10 +47,49 @@ class SortieFormType extends AbstractType
                 'mapped' => false])
             ->add('codePostal', EntityType::class, ['class' => Ville::class, 'choice_label' => 'codePostal',
                 'mapped' => false] )
-            //->add('etat', EntityType::class, ['class' =>Etat::class, 'choice_label' => 'libelle'])
-
+            /*************************************************************/
+            /*->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                'placeholder' => '',
+                'mapped' => false
+            ])*/
         ;
+
+        /*$formModifier = function (FormInterface $form, Lieu $lieu = null, Ville $ville = null) {
+            $lieux = null === $ville? [] : $ville->getLieus();
+
+            $form->add('Lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'placeholder' => '',
+                'choices' => $lieux,
+            ]);
+        };
+
+
+            $builder->addEventListener(
+                FormEvents::PRE_SET_DATA,
+                function (FormEvent $event) use ($formModifier){
+
+                    $data = $event->getData();
+                   $formModifier($event->getForm(), $data->getLieu()->getVille());
+
+
+                }
+            );
+
+            $builder->get('ville')->addEventListener(
+                FormEvents::POST_SUBMIT,
+                function (FormEvent $event) use ($formModifier){
+                    $ville = $event->getForm()->getData();
+
+                    $formModifier($event->getForm()->getParent(), $ville);
+                }
+            );*/
+
     }
+
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
